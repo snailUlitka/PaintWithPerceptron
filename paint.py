7d7d90a2ce239b62baf80b7f6f63c7444a852d5a
snailUlitka <109751977+snailUlitka@users.noreply.github.com>
2024-03-01 14:16:06 +0400

    Feature : : Paint (#4)
    * Rename from `main` to `model`
    
    * Create simple paint app
    
    * Add recognize button
    
    * Update perceptron
    
    * Load model
    
    * Add save image method
    
    * Update model
    
    * Add result label
    
    * Add recognize method
    
    * Fix image size
    
    ---------
    
    Co-authored-by: Snail <mihail.polevoda@gmail.com>

---
+++ paint.py
@@ -0,0 +1,103 @@
+from tkinter import *
+import tensorflow as tf
+from PIL import ImageGrab, Image
+import numpy as np
+import tensorflow_datasets as tfds
+
+
+class Paint(object):
+
+    DEFAULT_PEN_SIZE = 10.0
+    DEFAULT_COLOR = 'black'
+
+    def __init__(self):
+        self.root = Tk()
+
+        self.pen_button = Button(
+            self.root, text='Pen', command=self.use_pen
+        )
+        self.pen_button.grid(row=0, column=0)
+
+        self.eraser_button = Button(
+            self.root, text='Eraser', command=self.use_eraser
+        )
+        self.eraser_button.grid(row=0, column=2)
+
+        self.recognize_button = Button(
+            self.root, text='Recognize', command=self.recognize
+        )
+        self.recognize_button.grid(row=0, column=4)
+
+        self.c = Canvas(self.root, bg='white', width=600, height=600)
+        self.c.grid(row=1, columnspan=5)
+
+        self.lbl = Label(text="Result: ...")
+        self.lbl.grid(row=2, column=2)
+
+        self.setup()
+        self.root.mainloop()
+
+    def setup(self):
+        self.old_x = None
+        self.old_y = None
+        self.color = self.DEFAULT_COLOR
+        self.eraser_on = False
+        self.active_button = self.pen_button
+        self.c.bind('<B1-Motion>', self.paint)
+        self.c.bind('<ButtonRelease-1>', self.reset)
+
+    def use_pen(self):
+        self.activate_button(self.pen_button)
+
+    def use_eraser(self):
+        self.activate_button(self.eraser_button, eraser_mode=True)
+
+    def activate_button(self, some_button, eraser_mode=False):
+        self.active_button = some_button
+        self.eraser_on = eraser_mode
+
+    def paint(self, event):
+        self.line_width = self.DEFAULT_PEN_SIZE
+        paint_color = 'white' if self.eraser_on else self.color
+
+        if self.old_x and self.old_y:
+            self.c.create_line(
+                self.old_x, self.old_y, event.x, event.y,
+                width=self.line_width, fill=paint_color,
+                capstyle=ROUND, smooth=TRUE, splinesteps=36
+            )
+
+        self.old_x = event.x
+        self.old_y = event.y
+
+    def reset(self, event):
+        self.old_x, self.old_y = None, None
+
+    def save_image(self):
+        x = self.root.winfo_rootx() + self.c.winfo_x()
+        y = self.root.winfo_rooty() + self.c.winfo_y()
+        x1 = x + self.c.winfo_width()
+        y1 = y + self.c.winfo_height()
+
+        ImageGrab.grab().crop((x, y, x1, y1)).resize(
+            (28, 28)).save('.\\imgaes_to_recognize\\img.png')
+
+    def normalize_img(self, image):
+        """Normalizes images: `uint8` -> `float32`."""
+        return tf.cast(image, tf.float32) / 255.
+
+    def recognize(self):
+        model = tf.keras.models.load_model('paint_percep')
+
+        self.save_image()
+        image = np.array(Image.open('.\\imgaes_to_recognize\\img.png'))
+        image = self.normalize_img(image)[np.newaxis, ...]
+        
+        result = model.predict(image[:, :, :, 0])
+        result = np.argmax(result[0], axis=-1)
+
+        self.lbl.config(text='Result: ' + str(int(result)))
+
+
+if __name__ == '__main__':
+    Paint()
